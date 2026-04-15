11a4b94360d5a3281fdabcabb0fb113cadb0caa2
Utkarsh7535 <utkarshguptatheindian@gmail.com>
2026-04-16 01:05:46 +0530

    Cleaned up development and testing artifacts for final submission

--- test_exchangeinfo.py
+++
@@ -1,5 +0,0 @@
-﻿import sys
-import requests
-resp = requests.get('https://testnet.binancefuture.com/fapi/v1/exchangeInfo')
-info = resp.json()
-print("Order Types for BTCUSDT:", [s['orderTypes'] for s in info['symbols'] if s['symbol'] == 'BTCUSDT'])
